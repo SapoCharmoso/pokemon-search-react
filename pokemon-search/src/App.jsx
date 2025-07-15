bc4a87779019fae136689793e4052f9967bbdcae
SapoCharmoso <charmyfrogy@gmail.com>
2025-07-15 16:06:36 -0300

    layout primario em contrução

--- pokemon-search/src/App.jsx
+++ pokemon-search/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="m-10 rounded bg-red-900 h-48 text-center p-4">
+        <h1 className="text-5xl">Pokemon Search</h1>
+        <form className="" action="">
+          <div id="form-row" className="grid gap-3">
+            <label className="text-2xl" htmlFor="">
+              Type your pokemon
+            </label>
+            <input
+              className="bg-white placeholder-gray-400 rounded h-10"
+              placeholder="Pesquise aqui"
+              type="text"
+            />
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="m-10 rounded bg-red-900 h-96 p-2 grid grid-cols-8 gap-8">
+        <div>
+          <div className="h-40 w-40 text-amber-950 bg-white rounded">
+            <img className="h-30 w-30" src="/public/vite.svg" alt="" />
+          </div>
+          <div className="">
+            <p className="text-sm">#0001</p>
+            <h1 className="">Vite Logo</h1>{" "}
+            {/*Alterar o tamanho do nome do pokemon e colocar os tipos em baixo do nome dele*/}
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
